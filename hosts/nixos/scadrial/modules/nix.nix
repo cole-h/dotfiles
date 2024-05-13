9a33d4e56d6b1a384d628303c3de124529328689
Cole Helbling <cole.e.helbling@outlook.com>
2024-05-13 09:55:04 -0700

    scadrial/nix: cleanup trusted public keys

--- hosts/nixos/scadrial/modules/nix.nix
+++ hosts/nixos/scadrial/modules/nix.nix
@@ -8,17 +8,9 @@
 
       trusted-users = [ "vin" ];
       substituters = [
-        # "https://cache.qyliss.net"
-        # "https://cole-h.cachix.org"
-        # "https://nixpkgs-wayland.cachix.org"
-        # "https://nix-community.cachix.org"
       ];
 
       trusted-public-keys = [
-        "qyliss-x220:bZQtoCyr68idLFb8UQeDjnjitO/xAj52gOo9GoKZuog="
-        "cole-h.cachix.org-1:qmEJ4uAe5tWwFxU/U5T/Nf2+wzXM3/rCP0SIGbK0dgU="
-        "nixpkgs-wayland.cachix.org-1:3lwxaILxMRkVhehr5StQprHdEo4IrE8sRho9R9HOLYA="
-        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
       ];
     };
   };
