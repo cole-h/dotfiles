e245a07ad3eaf997e9de27466da1e81925a05429
Cole Helbling <cole.e.helbling@outlook.com>
2024-12-05 09:50:40 -0800

    wip

--- hosts/darwin/default.nix
+++ hosts/darwin/default.nix
@@ -78,17 +78,18 @@ let
               internal = true;
             };
 
-            options.nix.configureBuildUsers = lib.mkOption {
-              type = lib.types.bool;
-              default = false;
-              internal = true;
-            };
+            # options.nix.configureBuildUsers = lib.mkOption {
+            #   type = lib.types.bool;
+            #   default = false;
+            #   internal = true;
+            # };
 
             options.nixpkgs.flake = lib.mkOption {
               internal = true;
             };
 
             config.system.activationScripts.nix-daemon.text = "";
+
             config.system.checks.verifyNixChannels = false;
           })
         ];
